deddfa4ca8860f417287959866eae9c6f1b123db
sangh <sangh@AMANPC>
2026-02-11 14:42:19 -0500

    initial commit

---
+++ src/main/java/com/mycompany/ice5gitaction_amansangha/ICE5GitAction_AmanSangha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice5gitaction_amansangha;
+
+/**
+ *
+ * @author sangh
+ */
+public class ICE5GitAction_AmanSangha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
