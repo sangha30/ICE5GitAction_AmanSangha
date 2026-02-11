56710cfd5ad21ab8945e2923d9970d6cb1377918
sangh <sangh@AMANPC>
2026-02-11 14:49:46 -0500

    print my name

--- src/main/java/com/mycompany/ice5gitaction_amansangha/ICE5GitAction_AmanSangha.java
+++ src/main/java/com/mycompany/ice5gitaction_amansangha/ICE5GitAction_AmanSangha.java
@@ -12,5 +12,6 @@ public class ICE5GitAction_AmanSangha {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("My name is Aman Sangha!");
     }
 }
